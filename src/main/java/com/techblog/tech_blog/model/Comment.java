63fa50ab30d9576ba1ef4e8c7028d1e4ebed040d
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-26 23:48:28 -0300

    Criação das classes de entidades: User, Post, Comment e Like

---
+++ src/main/java/com/techblog/tech_blog/model/Comment.java
@@ -0,0 +1,44 @@
+package src.main.java.com.techblog.tech_blog.model;
+
+public class Comment {
+    private Long id;
+    private String content;
+    private Post post;
+    private User user;
+
+    public Comment() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/techblog/tech_blog/model/Like.java
@@ -0,0 +1,35 @@
+package src.main.java.com.techblog.tech_blog.model;
+
+public class Like {
+    private Long id;
+    private Post post;
+    private User user;
+
+    public Like() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
